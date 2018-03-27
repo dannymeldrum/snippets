923a668483922432f7746f7042a255bcc4a125d2
Danny Meldrum <danny.meldrum@stickyeyes.com>
2018-03-27 02:42:45 +0100

    config file for api keys

--- google-maps-api-sheets/index.js
+++ google-maps-api-sheets/index.js
@@ -1,8 +1,19 @@
 var map;
 var locations = [];
+var key = config.API_KEY;
+var sheet = config.SHEET_ID;
 
-function initialiseMap() {
-  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/{{SHEET_ID}}/values/Sheet1!A2:D?key={{API_KEY}}", function(data) {
+window.onload = loadScript;
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3&key='+key+'&callback=initialize';
+  document.body.appendChild(script);
+}
+
+function initialize() {
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/"+sheet+"/values/Sheet1!A2:D?key="+key+"", function(data) {
     	$(data.values).each(function() {
     		var location = {};
 				location.title = this[0];
@@ -16,6 +27,7 @@ function initialiseMap() {
         zoom: 10,
         center: new google.maps.LatLng(0, 0)
       };
+
       var map = new google.maps.Map(document.getElementById('map'), mapOptions);
       setLocations(map, locations);
   });
@@ -26,6 +38,7 @@ function setLocations(map, locations) {
   var infowindow = new google.maps.InfoWindow({
     content: "Content String"
   });
+
   for (var i = 0; i < locations.length; i++) {
     var new_marker = createMarker(map, locations[i], infowindow);
     bounds.extend(new_marker.position);
@@ -38,15 +51,18 @@ function createMarker(map, location, infowindow) {
     lat: parseFloat(location.latitude),
     lng: parseFloat(location.longitude)
   };
+
   var marker = new google.maps.Marker({
     position: position,
     map: map,
     title: location.title,
   });
+
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.setContent('<div><strong>' + location.title  + '</strong></div>' +
     '<div>' + location.address  + '</div>');
     infowindow.open(map, marker);
   });
+
   return marker;
 }
